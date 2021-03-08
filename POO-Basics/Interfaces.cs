951d8db3502183ee23a660bf480b393c71503ed8
poxx1 <julianlastrama@outlook.com>
2021-03-08 03:59:58 -0300

    Interfaces Corregido

--- POO-Basics/Interfaces.cs
+++ POO-Basics/Interfaces.cs
@@ -4,12 +4,20 @@ using System.Text;
 
 namespace POO_Basics
 {
-    public interface Interfaz
-    {
-
-    }
-    public class Interfaces:Interfaz
-    {
-
+         //Toda clase que utilice la interfaz esta obligado a utilizar todos sus Metodos. 
+        public interface IBebida { 
+        public string Marca { get; set; } 
+        public int contenidoLitros { get; set; } 
+    } 
+    
+        public class Bebidas:IBebida {
+        public string Marca { get; set; } 
+        public int contenidoLitros { get; set; } 
+        public void Asignar() { Marca = "Quilmes"; contenidoLitros = 1; } 
+    } 
+    public class Vino : IBebida { 
+        public string Marca { get; set; } 
+        public int contenidoLitros { get; set; } 
+        public void Asingar() { Marca = "Santa Julia"; contenidoLitros = 1; } 
     }
 }
